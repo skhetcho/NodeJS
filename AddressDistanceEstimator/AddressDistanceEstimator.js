3fc5865dc73479a9457ca85b3089397b48f51064
skhetcho <khetcho@gmail.com>
2020-10-26 10:45:55 -0700

    modified Google API query

--- AddressDistanceEstimator/AddressDistanceEstimator.js
+++ AddressDistanceEstimator/AddressDistanceEstimator.js
@@ -1,6 +1,14 @@
 //get the data
 const data = require('./data');
 
+//distance information calculator
+var distance = require('google-distance');
+distance.apiKey = 'AIzaSyB5-UM8ijEF4r1txLMYUtwdgw120BEUxNY';
+
+//excel library
+var excel = require('excel4node');
+
+
 //organize it in an array based on the # of lines
 let array = data.dumpAddresses.split('\n');
 
@@ -30,5 +38,60 @@ for (var i = 0; i < array.length; i++) {
     }
 }
 
-console.log("pickupLocation: " + pickupLocation.length);
-console.log("dropoffLocation: " + dropoffLocation.length);
+let distanceValueMeters = [];
+let distanceKms = [];
+let durationValueSeconds = [];
+let durationMinutes = []
+
+for (var j = 0; j < 3; j++) {
+    distance.get(
+        {
+            origin: pickupLocation[j],
+            destination: dropoffLocation[j]
+        },
+        async function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            const dataDis = await data.distanceValue;
+            distanceValueMeters.push(dataDis);
+
+            const dataDist = await data.distance; 
+            distanceKms.push(dataDist);
+
+            const dataTime = await data.durationValue;
+            durationValueSeconds.push(dataTime);
+
+            const dataTim = await data.duration; 
+            durationMinutes.push(dataTim);
+        }
+    );
+}
+setTimeout(function(){
+    console.log("pickupLocation: " + pickupLocation.length);
+    console.log("dropoffLocation: " + dropoffLocation.length);
+    console.log("distanceValueMeters: " + distanceValueMeters.length);
+    console.log("distanceKms: " + distanceKms.length);
+    console.log("durationValueSeconds: " + durationValueSeconds.length);
+    console.log("durationMinutes: " + durationMinutes.length);
+
+    console.log("distanceValueMeters: " + distanceValueMeters[0]);
+    console.log("distanceKms: " + distanceKms[0]);
+    console.log("durationValueSeconds: " + durationValueSeconds[0]);
+    console.log("durationMinutes: " + durationMinutes[0]);
+    
+}, 3000)
+
+
+
+// // Create a new instance of a Workbook class
+// var workbook = new excel.Workbook();
+
+// // Add Worksheets to the workbook
+// var worksheet = workbook.addWorksheet('Day Data');
+// // (row, column)
+// worksheet.cell((j + 1), 1).number(data.distanceValue);
+// worksheet.cell((j + 1), 2).string(data.distance);
+// worksheet.cell((j + 1), 3).number(data.durationValue);
+// worksheet.cell((j + 1), 4).string(data.duration);
+// workbook.write('DayAnalyzedDrivingData.xlsx');
